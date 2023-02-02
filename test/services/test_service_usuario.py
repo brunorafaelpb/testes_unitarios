3745f111372a302792e41cc99ee5ef9757da8385
brad <brad@cesar.org.br>
2023-02-02 14:11:51 -0300

    Melhoria nos testes da funcionalidade remover usuário

--- test/services/test_service_usuario.py
+++ test/services/test_service_usuario.py
@@ -14,6 +14,7 @@ class TestServiceUsuario:
         self.ID_inexistente = 999
         self.ID_invalido = "1"
         self.ID_none = None
+        self.store_vazio = []
 
     def test_add_usuario_valido(self):
         resultado_esperado = "Usuário adicionado"
@@ -24,31 +25,27 @@ class TestServiceUsuario:
 
     def test_add_usuario_nome_valido_profissao_invalida(self):
         resultado_esperado ="Usuário inválido"
-        store_esperado = []
         result = self.service.add_usuario(self.nome_valido, self.profissao_int)
         assert (result == resultado_esperado)
-        assert (self.service.store.bd == store_esperado)
+        assert (self.service.store.bd == self.store_vazio)
 
     def test_add_usuario_nome_invalida_profissao_valido(self):
         resultado_esperado = "Usuário inválido"
-        store_esperado = []
         result = self.service.add_usuario(self.nome_int, self.profissao_valida)
         assert (result == resultado_esperado)
-        assert (self.service.store.bd == store_esperado)
+        assert (self.service.store.bd == self.store_vazio)
 
     def test_add_usuario_nome_valido_profissao_none(self):
         resultado_esperado = "Usuário inválido"
-        store_esperado = []
         result = self.service.add_usuario(self.nome_valido, self.profissao_none)
         assert (result == resultado_esperado)
-        assert (self.service.store.bd == store_esperado)
+        assert (self.service.store.bd == self.store_vazio)
 
     def test_add_usuario_nome_nome_profissao_valido(self):
         resultado_esperado = "Usuário inválido"
-        store_esperado = []
         result = self.service.add_usuario(self.nome_none, self.profissao_valida)
         assert (result == resultado_esperado)
-        assert (self.service.store.bd == store_esperado)
+        assert (self.service.store.bd == self.store_vazio)
 
     def test_list_usuarios_valido(self):
         self.service.add_usuario(self.nome_valido, self.profissao_valida)
@@ -85,24 +82,28 @@ class TestServiceUsuario:
         cadastrado = self.service.add_usuario(self.nome_valido, self.profissao_valida)
         result = self.service.remove_usuario_id(self.ID_valido)
         assert (result == resultado_esperado)
+        assert (self.service.store.bd == self.store_vazio)
 
     def test_remover_usuario_ID_inexistente(self):
         resultado_esperado = "Usuário não encontrado"
         cadastrado = self.service.add_usuario(self.nome_valido, self.profissao_valida)
         result = self.service.remove_usuario_id(self.ID_inexistente)
         assert (result == resultado_esperado)
+        assert (self.service.store.bd > self.store_vazio)
 
     def test_remover_usuario_ID_none(self):
         resultado_esperado = "ID inválido"
         cadastrado = self.service.add_usuario(self.nome_valido, self.profissao_valida)
         result = self.service.remove_usuario_id(self.ID_none)
         assert (result == resultado_esperado)
+        assert (self.service.store.bd > self.store_vazio)
 
     def test_remover_usuario_ID_string(self):
         resultado_esperado = "ID inválido"
         cadastrado = self.service.add_usuario(self.nome_valido, self.profissao_valida)
         result = self.service.remove_usuario_id(self.ID_invalido)
         assert (result == resultado_esperado)
+        assert (self.service.store.bd > self.store_vazio)
 
     # TODO Criar testes para as funções em service_usuario
     # Falta completar os testes para atualizar usuario
